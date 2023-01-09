207af3ca6b6bb62c8241add38b3ce7bd4e2504ac
Nahid345 <93212220+Nahid345@users.noreply.github.com>
2023-01-09 22:20:36 +0600

    first commit

---
+++ src/components/Product/Product.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import './Product.css'
+const Product = ({product, handleAddToCart}) => {
+    // const{product, handleAddToCart} = props;
+    const {img,name,price,seller,ratings} = product;
+    return (
+        <div className='product'>
+            <img src={img} alt=""></img>
+           <div className='product-info'>
+            <p className='product-name'>{name}</p>
+            <p>Price: ${price}</p>
+            <p><small>Seller: {seller}</small></p>
+            <p><small>Ratings: {ratings} starts</small></p>
+           </div>
+           <button onClick={()=>handleAddToCart(product)} className='btn-cart'>
+            <p className='btn-text'>Add to Cart</p>
+            <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+           </button>
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css'
+
+const Shop = () => {
+
+    const [products,setProducts] =useState([]);
+    const [cart,setCart] = useState([]);
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+    },[])
+
+    const handleAddToCart = (product)=>{
+        console.log(product);
+        const newCart = [...cart,product]
+        setCart(newCart)
+        
+    }
+
+    return (
+        <div className='shop-container'>
+            <div className="products-conatiner">
+            {
+                products.map(product => <Product
+                key ={product.id}
+                product={product}
+                handleAddToCart = {handleAddToCart}
+                
+                ></Product>)
+            }
+            </div>
+            <div className="cart-conatiner">
+                <h4>Selected items: {cart.length}</h4>
+
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
